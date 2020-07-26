f2efd7355a7d083e22dbb89f011baaa028e4b1bb
Haroon <m.haroon.s@protonmail.com>
2020-07-26 22:38:09 +0500

    login system estabilished

--- thenxparser/thenx/views.py
+++ thenxparser/thenx/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 from django.http import HttpResponse
 
-
+@login_required(login_url='thenx:login')
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Welcome to the THENX APP.")
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect('thenx:index')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'thenx/login.html', {'form':form})
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('thenx')
