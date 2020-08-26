99c5be2c8ac468aaa3598dcbf97025ebece2c9a7
Haroon <m.haroon.s@protonmail.com>
2020-08-27 02:10:47 +0500

    updated wsgi for hostbreak

--- thenxparser/thenxparser/wsgi.py
+++ thenxparser/thenxparser/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 from dj_static import Cling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thenxparser.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = Cling(get_wsgi_application())
