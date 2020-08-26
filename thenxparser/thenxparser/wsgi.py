d57367b9ca639660120333c80f0188f74c5e4148
Haroon <m.haroon.s@protonmail.com>
2020-08-27 02:34:30 +0500

    updated wsgi for hostbreak

--- thenxparser/thenxparser/wsgi.py
+++ thenxparser/thenxparser/wsgi.py
@@ -8,10 +8,19 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
 from dj_static import Cling
+
+# add project folder to path
+path = '/home/mharoons/thenx/repositories/vetter/thenxparser/'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+path = '/home/mharoons/thenx'
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thenxparser.settings')
 
