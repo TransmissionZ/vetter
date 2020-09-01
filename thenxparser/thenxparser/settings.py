ae441d7420558ef43f17320c88245674e53c66d7
Haroon <m.haroon.s@protonmail.com>
2020-09-02 03:19:48 +0500

    added allowed_host

--- thenxparser/thenxparser/settings.py
+++ thenxparser/thenxparser/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'k!nbt7iah8pw_!o06my%jqx*n*)+72jhbjt)!-onqs@xn^b@6u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["transmissionz.pythonanywhere.com", '127.0.0.1']
+ALLOWED_HOSTS = ["162.0.226.237", "transmissionz.pythonanywhere.com", '127.0.0.1']
 
 
 # Application definition
