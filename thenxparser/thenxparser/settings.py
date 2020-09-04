1c71a87721348c6ec407ac6a041182d7c4bb950d
Haroon <m.haroon.s@protonmail.com>
2020-09-05 04:00:31 +0500

    changed settings.py

--- thenxparser/thenxparser/settings.py
+++ thenxparser/thenxparser/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['.thenxb2b.ro', 'p.thenxb2b.ro', '188.213.33.123', 'mharoons.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.thenxb2b.ro', '188.213.33.123', 'mharoons.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
